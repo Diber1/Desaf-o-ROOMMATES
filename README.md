086f76a9eff8fe025722ea79cfd6b3ca8c3d1730
Diego Bertolotto Ramos <diegofranber@gmail.com>
2024-06-10 14:06:34 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Desaf-o-ROOMMATES
-MÓDULO 8 / Implementación API Backend Node Express / API REST
+# Desafío Seguimiento gastos
+
+Se creó una rama huerfana para usar plantilla
+
+```bash
+# en terminal se crea nueva rama con para definirla huerfana
+git checkout --orphan <nombre-rama>
+
+# se trabajó e hizo commits como siempre, para luego hacer push a una nueva rama de github
+git push --set-upstream origin <nombre-rama>
+
+```
---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+const app = express();
+
+// IMPORTACIÓN DE RUTAS
+import { router as users } from "./routes/users.js";
+import { router as home } from "./routes/index.js";
+import { router as expenses } from "./routes/expenses.js";
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/", home);
+app.use("/roommates", users);
+app.use("/gastos", expenses);
+
+app.listen(3000, () => {
+  console.log("App en puerto 3000");
+});
